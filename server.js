02e3c46684075347df7ef161fa1ee1e9f6c8e418
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-09-26 11:43:33 -0400

    Test connection to MongoDB

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const app = express()
+const { MongoClient } = require('mongodb')
+const dotenv = require('dotenv')
+dotenv.config()
+const uri = process.env.ATLAS_URI
+const client = new MongoClient(uri)
 const PORT = process.env.PORT || 3222
+let connectedClient, db 
+async function connectToMongoDB(){
+    try {
+        connectedClient = await client.connect()
+        console.log('Server connected to MongoDB!')
+    } catch(err) {
+        console.log(err)
+    } finally {
+        db = connectedClient.db('elmarwinelist-main-db-0cc35737ece')
+        app.listen(PORT, ()=>{
+            console.log(`Server is now listening on port: ${PORT}`)
+        })
+    }
+}
+connectToMongoDB()
+app.set('view engine','ejs')
 app.use(express.static('public'))
 
 app.get('/',(req, res)=>{
-    res.sendFile(index.html)
-})
-app.listen(PORT,()=>{
-    console.log(`Express Server Now Listening on port ${PORT}`);
+    db.collection('test').find().toArray()
+    .then(data=>{
+        res.render('index.ejs',{info:data})
+    })
 })
